995cb5851745982472f8607ade131d15a9592281
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-26 18:40:04 +0530

    Update entrypoint.sh

--- Information-PR/entrypoint.sh
+++ Information-PR/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-python requests_github.py
+
 
 git config --global user.email "sahilchilana@gmail.com"
 git config --global user.name "sahilchilana"
+git checkout develop
+python requests_github.py
+
 
 git add test_template.yaml
 git commit -m "Created test sam template for new additions"
