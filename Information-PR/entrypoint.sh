1b758eec784e2d907dba26385bb0480a439229f1
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-27 13:20:22 +0530

    Update entrypoint.sh

--- Information-PR/entrypoint.sh
+++ Information-PR/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION=us-east-1
 git config --global user.email "sahilchilana@gmail.com"
 git config --global user.name "sahilchilana"
 git checkout develop
