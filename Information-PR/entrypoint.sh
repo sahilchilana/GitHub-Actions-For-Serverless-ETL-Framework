e4de5f1ad3da7d4b02553fa14ec75d7bee2f9f46
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-07-01 14:52:06 +0530

    Update entrypoint.sh

--- Information-PR/entrypoint.sh
+++ Information-PR/entrypoint.sh
@@ -8,4 +8,6 @@ ls
 python send_email.py
 python requests_github.py
 sam validate --template /github/workspace/test_template.yaml
+cd ServerlessETLFramework/vocabularies/<vocabulary-name>/Scrapper
+python scrapper.py
 echo "Success!!!"
