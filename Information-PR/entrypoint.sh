5825bb30a274cf13c7e00497263ded05ac303d55
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-07-01 13:18:10 +0530

    Update entrypoint.sh

--- Information-PR/entrypoint.sh
+++ Information-PR/entrypoint.sh
@@ -3,11 +3,9 @@
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=us-east-1
-git config --global user.email "sahilchilana@gmail.com"
-git config --global user.name "sahilchilana"
-git checkout develop
+git branch
+ls
 python send_email.py
 python requests_github.py
 sam validate --template /github/workspace/test_template.yaml
-sam build && sam local invoke
 echo "Success!!!"
