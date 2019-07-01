8f234f8196dc49770e394051438c54b9923a0776
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-07-01 15:16:15 +0530

    Update entrypoint.sh

--- Information-PR/entrypoint.sh
+++ Information-PR/entrypoint.sh
@@ -3,11 +3,10 @@
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=us-east-1
+cat $GITHUB_EVENT_PATH
 git branch
 ls
 python send_email.py
 python requests_github.py
 sam validate --template /github/workspace/test_template.yaml
-cd ServerlessETLFramework/vocabularies/<vocabulary-name>/Scrapper
-python scrapper.py
 echo "Success!!!"
