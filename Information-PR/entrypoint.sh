ee4dfee110db595ffcc1aa499d6c6908039abe02
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-07-01 17:23:16 +0530

    Update entrypoint.sh

--- Information-PR/entrypoint.sh
+++ Information-PR/entrypoint.sh
@@ -7,7 +7,8 @@ data=$(cat $GITHUB_EVENT_PATH)
 data1=$(echo $data| jq .pull_request.head.repo.clone_url| tr -d '"' | cut -b 1-8)
 data2=$(echo $data| jq .pull_request.head.repo.clone_url| tr -d '"' | cut -b 9-)
 data=$data1$PERSONAL_ACCESS_TOKEN'@'$data2
-git clone $data forked-repo
-cd forked-repo
-git checkout template
-ls
+echo $data
+#git clone $data forked-repo
+#cd forked-repo
+#git checkout template
+#ls
