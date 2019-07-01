70321671115c96847aa546826b013312fced3505
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-07-01 16:05:17 +0530

    Update entrypoint.sh

--- Information-PR/entrypoint.sh
+++ Information-PR/entrypoint.sh
@@ -3,10 +3,9 @@
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=us-east-1
-cat $GITHUB_EVENT_PATH
-git branch
+data=$(cat $GITHUB_EVENT_PATH)
+data=$(echo $data| jq .pull_request.head.repo.clone_url)
+git clone $data forked-repo
+cd forked-repo
+git checkout template
 ls
-python send_email.py
-python requests_github.py
-sam validate --template /github/workspace/test_template.yaml
-echo "Success!!!"
