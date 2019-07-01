dbfcffb23c9cfc815f35e243b5f788fb2713cbd6
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-07-01 17:42:09 +0530

    Update entrypoint.sh

--- Information-PR/entrypoint.sh
+++ Information-PR/entrypoint.sh
@@ -6,7 +6,7 @@ export AWS_DEFAULT_REGION=us-east-1
 data=$(cat $GITHUB_EVENT_PATH)
 data1=$(echo $data| jq .pull_request.head.repo.clone_url| tr -d '"' | cut -b 1-8)
 data2=$(echo $data| jq .pull_request.head.repo.clone_url| tr -d '"' | cut -b 9-)
-data=$data1$PERSONAL_ACCESS_TOKEN'@'$data2
+data=$data1'sahilchilana:'$PASSWORD_GIT'@'$data2
 echo $data
 #git clone $data forked-repo
 #cd forked-repo
