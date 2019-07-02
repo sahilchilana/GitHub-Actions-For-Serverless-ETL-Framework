9222a451895913db524801e3dd2818d5591b2ed2
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-07-02 15:49:16 +0530

    Update entrypoint.sh

--- StateMachine_Execution/entrypoint.sh
+++ StateMachine_Execution/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION=us-east-1
+
 execution_name=$(date)
 execution_name=(${execution_name// /_})
 execution_name=(${execution_name//:/-})
