d9303f52102362b6f4d11eb753026691184bc9d5
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-07-02 15:38:04 +0530

    Update entrypoint.sh

--- Package&Deploy/entrypoint.sh
+++ Package&Deploy/entrypoint.sh
@@ -25,28 +25,3 @@ fi
 
 sam package --template-file test_template.yaml --output-template-file out.yml --s3-bucket serverlessetlframeworktestbucket
 sam deploy --stack-name serverlessetlframeworkteststack --capabilities CAPABILITY_IAM --template-file out.yml
-
-execution_name=$(date)
-execution_name=(${execution_name// /_})
-execution_name=(${execution_name//:/-})
-
-arn_value=$(aws cloudformation describe-stack-resources --stack-name serverlessetlframeworkteststack)
-arn_value=$(echo $arn_value |jq '.StackResources[] | select(.ResourceType == "AWS::StepFunctions::StateMachine").PhysicalResourceId' | tr -d '"')
-
-execution_arn=$(aws stepfunctions start-execution --state-machine $arn_value --name $execution_name| jq .executionArn | tr -d '"')
-
-sleep 30s
-
-outpt=$(aws stepfunctions describe-execution --execution-arn $execution_arn)
-output=$(echo $output| jq .status| tr -d '"')
-
-echo "Output='$output'"
-if [ "$output" == "SUCCEEDED" ]; then
-  echo "Test Passed"
-  python send_email.py $output
-
-else
-  echo "Test Failed"
-  python send_email.py $output
-
-fi
